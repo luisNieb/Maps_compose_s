0c8b3cac3f31dd614de8490f173bf912aab3ed88
luis alberto Alvarez <luisslaycore@gmail.com>
2024-04-18 00:34:46 -0600

    datas clases para consultar la api de google que nos retorna las cordenadas de un lugar segun la busqueda que hagamos

---
+++ app/src/main/java/com/example/maps_compose_s/models/GoogleGeoResults.kt
@@ -0,0 +1,19 @@
+package com.example.maps_compose_s.models
+
+data class GoogleGeoResults(
+    val results: List<Results>
+)
+
+data class Results(
+    val geometry: Geometry,
+    val formatted_address:String
+)
+
+data class Geometry(
+    val location: Location
+)
+
+data class Location(
+    val lat: Double,
+    val lng: Double
+)
---
+++ app/src/main/java/com/example/maps_compose_s/models/LocationModel.kt
@@ -0,0 +1,8 @@
+package com.example.maps_compose_s.models
+
+import com.google.android.gms.maps.model.LatLng
+
+data class LocationModel(
+    val name:String,
+    val coordenadas:LatLng
+)
